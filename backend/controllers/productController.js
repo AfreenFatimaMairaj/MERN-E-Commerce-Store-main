a5081fd485301ab96ad47bf59bb17f391c17ffba
afreen fatima <afreenmairaj786@gmail.com>
2025-04-26 08:34:35 -0700

    Initialize MERN E-commerce website project structure

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -30,59 +30,6 @@ const addProduct = asyncHandler(async (req, res) => {
   }
 });
 
-// const addProduct = asyncHandler(async (req, res) => {
-//   try {
-//     const {
-//       name,
-//       description,
-//       price,
-//       category,
-//       quantity,
-//       brand,
-//       countInStock,
-//       image,
-    // } = req.body;
-
-    // // Validation
-    // switch (true) {
-    //   case !name:
-    //     return res.json({ error: "Name is required" });
-    //   case !brand:
-    //     return res.json({ error: "Brand is required" });
-    //   case !description:
-    //     return res.json({ error: "Description is required" });
-    //   case !price:
-    //     return res.json({ error: "Price is required" });
-    //   case !category:
-    //     return res.json({ error: "Category is required" });
-    // //   case !quantity:
-    //     return res.json({ error: "Quantity is required" });
-    //   case !image:
-    //     return res.json({ error: "Image is required" });
-    // }
-
-    // const product = new Product({
-    //   name,
-    //   description,
-//       price,
-//       category,
-//       quantity,
-//       brand,
-//       countInStock,
-//       image,
-//     });
-
-//     await product.save();
-//     res.json(product);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(400).json(error.message);
-//   }
-// });
-
-
-
-
 
 const updateProductDetails = asyncHandler(async (req, res) => {
   try {
